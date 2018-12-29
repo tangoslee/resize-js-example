7573b3b029968b7eff0440883c9931c4a8b351f8
Sangmin Lee <tangoslee@gmail.com>
2018-12-29 02:46:44 -0500

    add pdf resize by PDF.js

--- upload.php
+++ upload.php
@@ -1,14 +1,21 @@
 <?php
+$storage = __DIR__ . '/uploads';
+$imgs = $_POST['img'] ?? [];
 
-$img = $_POST['img'];
-$img = str_replace('data:image/jpeg;base64,', '', $img);
-$img = str_replace(' ', '+', $img);
-$data = base64_decode($img);
-$file = __DIR__ . '/uploads/img-'.date("YmdHis").'.jpg';
-   
-if ($data && file_put_contents($file, $data)) {
-   echo "<p>save OK: $file.</p>";
-} else {
-   echo "<p>save NG.</p>";
-}   
+if (!file_exists($storage)) {
+    mkdir($storage);
+}
+
+foreach ($imgs as $img) {
+    $img = str_replace('data:image/jpeg;base64,', '', $img);
+    $img = str_replace(' ', '+', $img);
+    $data = base64_decode($img);
+    $file = $storage . '/' . uniqid('img-') . '.jpg';
+      
+    if ($data && file_put_contents($file, $data)) {
+        echo "<p>save OK: $file.</p>";
+    } else {
+        echo "<p>save NG.</p>";
+    }
+}
 echo "<pre>";print_r($_REQUEST);echo "</pre>";
